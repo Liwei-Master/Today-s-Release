ca4b4670cc63e6ebdb01158fc576f0e30eab78eb
Liwei <heliwei19950119@gmail.com>
2018-04-08 20:48:58 +1000

    homepage

---
+++ release/homepage/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url(r'^$', views.index, name="index"),
+    url(r'^today/recommendation', views.recommendation, name="recommendation"),
+    url(r'^today/interest', views.interest, name="interest"),
+    url(r'^today/hit', views.hit, name="interest"),
+
+]
